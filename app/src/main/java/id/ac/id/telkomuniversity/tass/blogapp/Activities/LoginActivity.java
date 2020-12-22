9f77f69e1ef284b1ab4f16ab9b92580ff99bb09a
MUHAMMAD ABIZARD <m.abizard1123@gmail.com>
2020-12-22 20:31:33 +0700

    blog app

--- app/src/main/java/id/ac/id/telkomuniversity/tass/blogapp/Activities/LoginActivity.java
+++ app/src/main/java/id/ac/id/telkomuniversity/tass/blogapp/Activities/LoginActivity.java
@@ -49,7 +49,7 @@ import com.facebook.appevents.AppEventsLogger;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //ini ya baru banget ljhoooo
+    //ini ya baru banget ljhoooo yaa
 
     private CallbackManager mCallbackManager;
 
